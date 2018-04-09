612a5e91fb381271e7b20c4bff79f6e6633a87e9
CyrusVatandoost <vatandoostcyrus@gmail.com>
2018-04-09 14:34:29 +0800

    Initial commit

---
+++ src/Driver.java
@@ -0,0 +1,8 @@
+
+public class Driver {
+
+	public static void main(String[] args) {
+		Philosopher p1 = new Philosopher();
+	}
+
+}
---
+++ src/Philosopher.java
@@ -0,0 +1,24 @@
+
+public class Philosopher {
+
+	public Philosopher() {
+		System.out.println("Philosopher created");
+	}
+	
+	public void checkLeft() {
+		
+	}
+	
+	public void checkRight() {
+		
+	}
+	
+	public void getLeft() {
+		
+	}
+	
+	public void getRight() {
+		
+	}
+	
+}
